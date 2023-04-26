5601980745c2f5383b5f63ff66b8bae755dc28b0
ivvist <ivvist@gmail.com>
2023-04-26 13:06:32 +0200

    web portals reseller

---
+++ checkpr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -H "Accept: application/vnd.github.antiope-preview+json" \
+     -H "Authorization: token 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" \
+     https://api.github.com/IVVORG/public-test-repo/commits/c6f1891/check-suites
+
