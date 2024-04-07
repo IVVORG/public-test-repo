d71df8a932d3b592ac8ecdc06ce6672c5fdd7a2b
ivvist <ivvist@gmail.com>
2024-04-07 14:50:49 +0200

    Update workflow name

--- make-cherry-pick.sh
+++ make-cherry-pick.sh
@@ -9,12 +9,11 @@ fi
 # Split the comma-separated list into an array of commit hashes
 IFS=',' read -r -a commit_hashes <<< "$1"
 
-# Checkout the "rc" branch
-git checkout rc || exit
 
 # Loop through the array and cherry-pick each commit from the "main" branch
 for commit_hash in "${commit_hashes[@]}"; do
     echo "Cherry-picking commit $commit_hash from main to rc..."
+    commit_hash=$(echo $commit_hash | tr -d ' ')
     git cherry-pick "$commit_hash" || {
         echo "Error cherry-picking commit $commit_hash. Aborting."
         exit 1
