a1f74c7fdab7b64a417f6d940810b99cfe8296e5
ivvist <ivvist@gmail.com>
2024-05-05 19:31:37 +0200

    Make release

--- .github/workflows/make-cherry-pick.sh
+++ .github/workflows/make-cherry-pick.sh
@@ -6,7 +6,12 @@ if [ -z "$branch" ];  then
   echo "Branch is not defined"
   exit 1
 fi
-git checkout -b $branch
+git clone -b $branch https://github.com/$org/$repo chp || {
+   echo "Something wnet wrong with clone branch"
+   exit 1
+}
+cd chp
+
 commit_hashes_arr="$commit_list"
 for word in $commit_hashes_arr; do
   # Append word to the list
@@ -49,7 +54,7 @@ for commit_hash in $sorted_commits; do
     echo "Cherry-picking commit $commit_hash from main to $branch..."
     echo $(git show -s --format=%ci $commit_hash) $commit_hash	
     commit_hash=$(echo $commit_hash | tr -d ' ')
-    git cherry-pick "$commit_hash" || {
+    git cherry-pick "$commit_hash" --allow-empty || {
         echo "Error cherry-picking commit $commit_hash. Aborting."
         exit 1
     }
