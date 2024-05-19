1d83596c7ce1d1e1d2272a6528e2f93853815d6f
ivvist <ivvist@gmail.com>
2024-05-19 19:55:10 +0200

    update workflow

---
+++ update-config-rc.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+repoHasRC() {
+  # Get tag last value
+  tagvalue=""
+  header="Accept: application/vnd.github+json"
+  repo_full_name="https://api.github.com/repos/${org}/${repo}/commits/main"
+  TAGS=$(curl -s "$repo_full_name/tags" | jq -r '.[].name')
+  # Filter tags with the specified prefix and postfix
+  TAG_PREFIX="v0."
+  TAG_POSTFIX=".1-rc"
+  tagvalue=$(echo "$TAGS" | grep "^$TAG_PREFIX.*$TAG_POSTFIX$")
+
+  if [ -z "$tagvalue" ]; then
+    echo "Last release candidate tag for repo github.com/${org}/${repo} not found."
+    exit 1
+  else
+    echo "Last release candidate tag for repo github.com/${org}/${repo} is tagvalue"
+  fi
+  # Remove prefix 'v0.' and postfix '.1-rc'
+  stripped_tagvalue=${tagvalue#v0.}
+  strdate=${stripped_tagvalue%.1-rc}
+
+  # Print the result
+  echo "The last rc tag date is: $strdate"
+  reptag="created"
+
+  pfound=0
+  while read -r line; do
+    i=$((i+1))
+    if [ $pfound -eq 0 ]; then 
+      if [[ $line =~ "pack: $pack" ]]; then
+  	pfound=1	
+      fi 
+    else
+       if [[ $line =~ "$updtag:" ]]; then
+         # Extract the date using grep and sed
+         curstrdate=$(echo "$line" | grep -oP '(?<=$reptag:\s*)\d+')
+         curdt=$(date -d "${curstrdate:0:8} ${curstrdate:8:2}:${curstrdate:10:2}" +"%s")
+         newdt=$(date -d "${strdate:0:8} ${strdate:8:2}:${strdate:10:2}" +"%s")
+         if [ "$newdt" -gt "$curdt" ]; ; then 
+           break
+         fi
+       fi
+    fi
+  done < ${stackfile}
+
+  return 1
+}
+
+stack="rc"
+stackfile="./airs-config-sync/stacks/$stack/stack.yml"
+if [ ! -f "$stackfile"  ]
+then
+    echo "File $stackfile not found "
+    exit 1
+fi
+
+reptag="created"
+packfound=0
+while read -r line; do
+  i=$((i+1))
+  if [ $packfound -eq 0 ]; then 
+    if [[ $line =~ "pack: $pack" ]]; then
+	packfound=1	
+    fi 
+  else
+     if [[ $line =~ "$updtag:" ]]; then
+       # Extract the date using grep and sed
+       sed -i "${i}s/.*$reptag.*/      $reptag: $tagvalue/" ${stackfile}
+       break
+     fi
+  fi
+done < ${stackfile}
+
