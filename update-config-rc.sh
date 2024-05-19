ca5cff97fb1c636cff2e6f207acba8f55baa4f97
ivvist <ivvist@gmail.com>
2024-05-19 21:20:22 +0200

    teset run release

--- update-config-rc.sh
+++ update-config-rc.sh
@@ -2,6 +2,9 @@
 
 repoHasRC() {
   # Get tag last value
+  org="untillpro"
+  repo=$2
+
   tagvalue=""
   header="Accept: application/vnd.github+json"
   repo_full_name="https://api.github.com/repos/${org}/${repo}/commits/main"
@@ -33,19 +36,38 @@ repoHasRC() {
   	pfound=1	
       fi 
     else
-       if [[ $line =~ "$updtag:" ]]; then
+       if [[ $line =~ "$reptag:" ]]; then
          # Extract the date using grep and sed
          curstrdate=$(echo "$line" | grep -oP '(?<=$reptag:\s*)\d+')
          curdt=$(date -d "${curstrdate:0:8} ${curstrdate:8:2}:${curstrdate:10:2}" +"%s")
          newdt=$(date -d "${strdate:0:8} ${strdate:8:2}:${strdate:10:2}" +"%s")
          if [ "$newdt" -gt "$curdt" ]; ; then 
-           break
+           return $newdt
          fi
        fi
     fi
   done < ${stackfile}
 
-  return 1
+  return "1"
+}
+
+updateSync() {
+  reptag="created"
+  packfound=0
+  while read -r line; do
+    i=$((i+1))
+    if [ $packfound -eq 0 ]; then 
+     if [[ $line =~ "pack: $pack" ]]; then
+  	packfound=1	
+     fi 
+    else
+       if [[ $line =~ "$reptag:" ]]; then
+       # Extract the date using grep and sed
+         sed -i "${i}s/.*$reptag.*/      $reptag: $tagvalue/" ${stackfile}
+         break
+       fi
+    fi
+  done < ${stackfile}
 }
 
 stack="rc"
@@ -55,21 +77,29 @@ then
     echo "File $stackfile not found "
     exit 1
 fi
+bp3tag=$(repoHasRC "airs-bp3" "bp3")
+if [[ $bp3tag == "1" ]]; then
+    echo "New rc tag for airs-bp3 does not exist"
+    exit 1
+fi
+botag=$(repoHasRC "airc-backoffice2" "resellerportal")
+if [[ $botag == "1" ]]; then
+    echo "New rc tag for airc-backoffice2 does not exist"
+    exit 1
+fi
+portaltag=$(repoHasRC "web-portals" "resellerportal")
+if [[ $portaltag == "1" ]]; then
+    echo "New rc tag for resellerportal does not exist"
+    exit 1
+fi
+portaltag=$(repoHasRC "web-portals" "paymentsportal")
+if [[ $portaltag == "1" ]]; then
+    echo "New rc tag for paymentsportal does not exist"
+    exit 1
+fi
 
-reptag="created"
-packfound=0
-while read -r line; do
-  i=$((i+1))
-  if [ $packfound -eq 0 ]; then 
-    if [[ $line =~ "pack: $pack" ]]; then
-	packfound=1	
-    fi 
-  else
-     if [[ $line =~ "$updtag:" ]]; then
-       # Extract the date using grep and sed
-       sed -i "${i}s/.*$reptag.*/      $reptag: $tagvalue/" ${stackfile}
-       break
-     fi
-  fi
-done < ${stackfile}
+updateSync $bp3tag
+updateSync $botag
+updateSync $portaltag
+
 
